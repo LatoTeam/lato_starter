796c2637a58dbe1df0e505f9d834beee33b0adbb
Gregorio Galante <gregogalante@gmail.com>
2016-09-09 00:34:58 +0200

    auto-commit

--- lib/lato_starter/interface.rb
+++ lib/lato_starter/interface.rb
@@ -1,8 +1,8 @@
 module LatoStarter
-  # Classe che contiene tutti i moduli che generano l'interfaccia del modulo
   module Interface
-    # Richiamo dati per l'accesso alle stringhe del modulo
+
     require 'lato_starter/interface/example'
     include LatoStarter::Interface::Example
+    
   end
 end
